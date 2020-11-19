7a6b1d7f9c1f5bb3d9300c73f3f98c698abc3eec
connellj <john.connell@niab.com>
2020-11-19 10:43:24 +0000

    update

--- SNP_calling_pileup/SNP_calling_pileup.sh
+++ SNP_calling_pileup/SNP_calling_pileup.sh
@@ -140,9 +140,19 @@ for Strain in C1 C2 C3 C4 C5 C6 C7 C8 C9 C10 C11 C12 C13 C14 C15 C16 C17 C18 C19
   done 
 done 
 
+#8.) Recalibrate bam files with recalibration table generated from previous step
 
-
-
+Reference=../../projects/fusarium_venenatum_miseq/genomes/WT/WT_contigs_unmasked.fa  
+for Strain in C1 C2 C3 C4 C5 C6 C7 C8 C9 C10 C11 C12 C13 C14 C15 C16 C17 C18 C19; do
+  for input in ../../projects/fusarium_venenatum_miseq/SNP_calling/F.venenatum/$Strain/alignment/nomulti/corrected_bam/"$Strain"_realigned.bam; do
+    recalibration_tables=/projects/fusarium_venenatum_miseq/SNP_calling/F.venenatum/$Strain/alignment/nomulti/base_recalibrate/"$Strain"_recal.table
+    echo $Strain
+    Outdir=/projects/fusarium_venenatum_miseq/SNP_calling/F.venenatum/$Strain/alignment/nomulti/base_recalibrate
+    mkdir -p $Outdir
+    ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/SNP_calling_pileup
+    sbatch $ProgDir/base_recalibrator.sh $Reference $Strain $input $recalibration_tables $Outdir
+  done 
+done 
 
 
 
