e7ee4bff719ace4665b24201456464d3406aed6d
connellj <john.connell@niab.com>
2020-11-26 19:15:14 +0000

    update

--- Circos/Cirocs_analysis.sh
+++ Circos/Cirocs_analysis.sh
@@ -3,8 +3,7 @@
 
 #1.) Create a .txt file with contig lengths of the genomes to be aligned. 
 #.txt file for genome 1 
-#must be running python v 3.6 or later, update with conda if not. 
-
+ 
 
 Genome1=$(ls ../../projects/fusarium_venenatum_miseq/genomes/WT/WT_contigs_unmasked.fa)
 OutDir=/home/connellj/Circos 
@@ -12,15 +11,12 @@ mkdir -p $OutDir
 ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/Circos
 $ProgDir/python_create_circos_file.py --genome $Genome1 --contig_prefix "A3_5_" > $OutDir/Fv_Illumina_genome.txt
 
-
 #.txt file for genome 2 
-
 
 Genome2=$(ls ../../projects/oldhome/connellj/local_MINion_Fv_genome/WT_albacore_v2_contigs_unmasked.fa)
 OutDir=/home/connellj/Circos 
 ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/Circos
 $ProgDir/python_create_circos_file.py --genome $Fv_MINion_genome --contig_prefix "A3_5_MIN" > $OutDir/Fv_MINion_genome.txt
-
 
 #concatenate files
 
@@ -28,9 +24,7 @@ File=/home/connellj/Circos
 cat $File/Fv_Illumina_genome.txt > $File/Fv_Fv_genome.txt
 tac $File/Fv_MINion_genome.txt >> $File/Fv_Fv_genome.txt
 
-
  #Contigs smaller than 10Kb can be removed if not required. 
-
 
   cat $OutDir/Fv_Fv_genome.txt \
   | grep -v -e "A3_5_contig_87" \
@@ -67,24 +61,15 @@ ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/Circos
 sbatch $ProgDir/satsuma_synteny.sh $Genome1 $Genome2 $OutDir 
 
 
-
-
-
-
-
-
-
-
-
-
-
 #3.) Run circos  
+# Circos relies on a configuration file which has other branching file involved in the ideogram configuration. 
 
-  ## Running CIRCOS --CONFIGURATION FILE LOCATION-- 
-  ## /home/connellj/software/circos
-OutDir=/home/connellj/fusarium_venenatum/circos_out/Fv_vs_Fv
-Conf=$(ls /home/connellj/git_repos/scripts/Fv_C-variants/Circos/Fv_Fv_circos.conf.sh)
-circos -conf $Conf -outputdir $OutDir
-mv $OutDir/circos.png $OutDir/Fv_Fv_circos.png
-mv $OutDir/circos.svg $OutDir/Fv_Fv_circos.svg
+
+Conf=/home/connellj/git_repos/scripts/Fv_C-variants/Circos/Fv_Fv_circos.conf.sh
+OutDir=/home/connellj/Circos/ideogram
+mkdir -p $OutDir
+ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/Circos
+sbatch $ProgDir/circos.sh $Conf $OutDir
+
+
 
