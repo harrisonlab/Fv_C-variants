8e2f51bcb8845cad0221c0899310fcc1504f1ad5
connellj <john.connell@niab.com>
2020-11-29 15:06:53 +0000

    update

--- Circos/Cirocs_analysis.sh
+++ Circos/Cirocs_analysis.sh
@@ -69,7 +69,7 @@ sbatch $ProgDir/satsuma_synteny.sh $Genome1 $Genome2 $OutDir
 
 Synteny_file=/home/connellj/Circos/satsuma_alignment/test_copy/satsuma_summary.chained.out            #Synteny file location and file name here  
 OutDir=/home/connellj/Circos/satsuma_alignment/test_copy/satsuma_summary_editedforcircos.chained.out  #Outfile location and file name here 
-pre1=A3_5_contig_  #change to your contig prefox
+pre1=A3_5_contig_  #change to your contig prefix
 pre2=A3_5_MIN_contig_  #change to your contig prefix
 ProgDir=/home/connellj/git_repos/emr_repos/Fv_C-variants/Circos
 $ProgDir/Python_edit_contig_names.py --synteny $Synteny_file --contig_prefix_1 $pre1 --contig_prefix_2 $pre2 --outfile $OutDir
